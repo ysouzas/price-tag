c8c55ae91d8f85f267fff046a3b38532f62dea15
Yago Souza Santos <6200092+ysouzas@users.noreply.github.com>
2026-05-15 12:32:38 +0100

    updates

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true,
- };
+  production: false,
+  supabaseUrl: 'https://ahnrwfdakkfunphkgtyu.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFobnJ3ZmRha2tmdW5waGtndHl1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3NzYyODgsImV4cCI6MjA5NDM1MjI4OH0.6SPaAnrrt54FsLphKwA9qU2sawXtCw6xHcNhmvDz5eM'
+};
